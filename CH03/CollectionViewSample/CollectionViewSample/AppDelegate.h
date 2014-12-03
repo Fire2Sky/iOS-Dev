60a87c8103880c774dd47329b2613a0a4a8efe8e
Fire2Sky <hackerbyty@gmail.com>
2014-12-04 01:27:19 +0800

    CollectionViewSample

---
+++ CH03/CollectionViewSample/CollectionViewSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewSample
+//
+//  Created by 田野 on 14/12/4.
+//  Copyright (c) 2014年 Fire2Sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CH03/CollectionViewSample/CollectionViewSample/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  CollectionViewSample
+//
+//  Created by 田野 on 14/12/4.
+//  Copyright (c) 2014年 Fire2Sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UICollectionViewController
+
+@property (strong, nonatomic) NSArray * evevnts;
+
+
+@end
+
