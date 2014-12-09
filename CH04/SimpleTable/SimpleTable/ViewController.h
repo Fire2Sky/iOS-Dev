6a284323196060b14042e3278618f72352deb58b
Fire2Sky <hackerbyty@gmail.com>
2014-12-09 20:19:21 +0800

    章节4写好了,但是显示的还是不完美,不能显示图片资源.

---
+++ CH04/SimpleTable/SimpleTable/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  SimpleTable
+//
+//  Created by 田野 on 14/12/9.
+//  Copyright (c) 2014年 Fire2Sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray * listTeams;
+
+
+@end
+
---
+++ CH04/SimpleTable/SimpleTable/ViewController.m
@@ -0,0 +1,72 @@
+//
+//  ViewController.m
+//  SimpleTable
+//
+//  Created by 田野 on 14/12/9.
+//  Copyright (c) 2014年 Fire2Sky. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    //读取数据文件信息
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *plistPath = [bundle pathForResource:@"team" ofType:@"plist"];
+    //信息存储在listTeams中
+    self.listTeams = [[NSArray alloc] initWithContentsOfFile:plistPath];
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+////转屏的时候响应的协议
+//- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+//{
+//    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+//}
+
+
+//实现数据协议
+//返回节的长度
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.listTeams count];
+}
+//返回单元格的索引
+-(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"CellIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil)
+    {
+        //cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:CellIdentifier];
+        
+    }
+    NSUInteger row = [indexPath row];
+    NSDictionary *rowDict = [self.listTeams objectAtIndex:row];
+    cell.textLabel.text = [rowDict objectForKey:@"name"];
+    
+    NSString *imagePath = [rowDict objectForKey:@"image"];
+    imagePath  = [imagePath stringByAppendingString:@"png"];
+    cell.imageView.image = [UIImage imageNamed:imagePath];
+    
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    return cell;
+    
+    
+}
+
+@end
