888d3aeb31797dd9485867bc2bd11be49dd633ed
Fire2Sky <hackerbyty@gmail.com>
2014-12-09 20:23:40 +0800

    现在显示完美了,但是只复制别人的代码啊!

--- CH04/SimpleTable/SimpleTable/ViewController.m
+++ CH04/SimpleTable/SimpleTable/ViewController.m
@@ -1,10 +1,75 @@
+////
+////  ViewController.m
+////  SimpleTable
+////
+////  Created by 田野 on 14/12/9.
+////  Copyright (c) 2014年 Fire2Sky. All rights reserved.
+////
 //
-//  ViewController.m
-//  SimpleTable
+//#import "ViewController.h"
 //
-//  Created by 田野 on 14/12/9.
-//  Copyright (c) 2014年 Fire2Sky. All rights reserved.
+//@interface ViewController ()
 //
+//@end
+//
+//@implementation ViewController
+//
+//- (void)viewDidLoad {
+//    [super viewDidLoad];
+//    
+//    
+//    //读取数据文件信息
+//    NSBundle *bundle = [NSBundle mainBundle];
+//    NSString *plistPath = [bundle pathForResource:@"team" ofType:@"plist"];
+//    //信息存储在listTeams中
+//    self.listTeams = [[NSArray alloc] initWithContentsOfFile:plistPath];
+//    
+//    // Do any additional setup after loading the view, typically from a nib.
+//}
+//
+//- (void)didReceiveMemoryWarning {
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
+//
+//////转屏的时候响应的协议
+////- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+////{
+////    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+////}
+//
+//
+////实现数据协议
+////返回节的长度
+//- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+//{
+//    return [self.listTeams count];
+//}
+////返回单元格的索引
+//-(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    static NSString *CellIdentifier = @"CellIdentifier";
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+//    if (cell == nil)
+//    {
+//        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:CellIdentifier];
+//        
+//    }
+//    NSUInteger row = [indexPath row];
+//    NSDictionary *rowDict = [self.listTeams objectAtIndex:row];
+//    cell.textLabel.text = [rowDict objectForKey:@"name"];
+//    
+//    NSString *imagePath = [rowDict objectForKey:@"image"];
+//    imagePath  = [imagePath stringByAppendingString:@"png"];
+//    cell.imageView.image = [UIImage imageNamed:imagePath];
+//    
+//    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+//    return cell;
+//    
+//    
+//}
+//
+//@end
 
 #import "ViewController.h"
 
@@ -14,59 +79,55 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     
-    
-    //读取数据文件信息
     NSBundle *bundle = [NSBundle mainBundle];
-    NSString *plistPath = [bundle pathForResource:@"team" ofType:@"plist"];
-    //信息存储在listTeams中
+    NSString *plistPath = [bundle pathForResource:@"team"
+                                           ofType:@"plist"];
+    //获取属性列表文件中的全部数据
     self.listTeams = [[NSArray alloc] initWithContentsOfFile:plistPath];
-    
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
 }
 
-////转屏的时候响应的协议
-//- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-//{
-//    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-//}
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
 
-
-//实现数据协议
-//返回节的长度
-- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+#pragma mark --UITableViewDataSource 协议方法
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return [self.listTeams count];
 }
-//返回单元格的索引
--(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"CellIdentifier";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil)
-    {
-        //cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:CellIdentifier];
-        
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:CellIdentifier];
     }
+    
+    
     NSUInteger row = [indexPath row];
     NSDictionary *rowDict = [self.listTeams objectAtIndex:row];
-    cell.textLabel.text = [rowDict objectForKey:@"name"];
+    cell.textLabel.text =  [rowDict objectForKey:@"name"];
     
     NSString *imagePath = [rowDict objectForKey:@"image"];
-    imagePath  = [imagePath stringByAppendingString:@"png"];
+    imagePath = [imagePath stringByAppendingString:@".png"];
     cell.imageView.image = [UIImage imageNamed:imagePath];
     
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
     return cell;
-    
-    
 }
 
+
 @end
+
